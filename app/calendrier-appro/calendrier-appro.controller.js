f17286fae00b2f084c5d0a57dc555c79046ed5de
unknown <isaacndri@gmail.com>
2024-07-26 00:59:19 +0000

    Revu calendrier appro

--- app/calendrier-appro/calendrier-appro.controller.js
+++ app/calendrier-appro/calendrier-appro.controller.js
@@ -11,7 +11,10 @@
     function CalendrierApproController ($scope, $state, FetchData, API_URL, AuthServerProvider, $filter) {
 
         var vm = this;
+        vm.produits = [];
         vm.inventaires = [];
+        vm.searchAnnee = '';
+        vm.typeRecherche = 'bymoisannee'
         var authToken = AuthServerProvider.getToken();
         vm.loadAll = function() {
             
@@ -25,6 +28,21 @@
                 console.log('nombre d\'élément ' + vm.totalElements);
                 console.log('nombre de page ' + vm.totalPage);
                 console.log("OUUHHH "+vm.inventaires);
+            }, function (error) {
+                console.log(error);
+            });
+
+
+            FetchData.getData(API_URL + 'api/produits')
+            .then(function (response) {
+                // console.log(response);
+                vm.produits = response.data;
+                // vm.totalElements = response.data.totalElements;
+                // vm.totalPage = response.data.totalPages;
+
+                // console.log('nombre d\'élément ' + vm.totalElements);
+                // console.log('nombre de page ' + vm.totalPage);
+                // console.log("OUUHHH "+vm.inventaires);
             }, function (error) {
                 console.log(error);
             });
@@ -47,17 +65,13 @@
             if (vm.searchQuery) {
                 var dateFormat = 'yyyy-MM-dd';
                 var searchQuery = $filter('date')(vm.searchQuery, dateFormat);
-                console.log(API_URL + 'api/search/'+searchQuery+'/inventaires?page=' + (vm.currentPage ?? 0) + '&size=' + (vm.pageSize ?? 3));
-                FetchData.getData(API_URL + 'api/search/'+searchQuery+'/inventaires?page=' + (vm.currentPage ?? 0) + '&size=' + (vm.pageSize ?? 3))
+                console.log(API_URL + 'api/_search/inventaire/'+searchQuery);
+                FetchData.getData(API_URL + 'api/_search/inventaire/'+searchQuery)
                 .then(function (response) {
                     console.log(response);
-                    vm.inventaires = response.data.content;
-                    vm.totalElements = response.data.totalElements;
-                    vm.totalPage = response.data.totalPages;
-
-                    console.log('nombre d\'élément ' + vm.totalElements);
-                    console.log('nombre de page ' + vm.totalPage);
-                    console.log("OUUHHH "+vm.inventaires);
+                    vm.inventaires = response.data;
+                    console.log(API_URL + 'api/_search/inventaire/ RESPONSE', response);
+                 
                 }, function (error) {
                     console.log(error);
                 });
@@ -65,6 +79,52 @@
             }
  
         };
+
+        vm.searchProduitAnnee = function () {
+            console.log("bouton cliqué", vm.searchAnnee);
+            if (vm.searchAnnee && vm.produit.id) {
+                var dateFormat = 'yyyy';
+                var searchAnnee = $filter('date')(vm.searchAnnee, dateFormat);
+                console.log(API_URL + 'api/_search/'+searchAnnee+'/inventaire/'+vm.produit.id);
+                FetchData.getData(API_URL + 'api/_search/'+searchAnnee+'/inventaire/'+vm.produit.id)
+                .then(function (response) {
+                    console.log(response);
+                    vm.inventaires = response.data;
+                    console.log(API_URL + 'api/_search/inventaire/ RESPONSE', response);
+                 
+                }, function (error) {
+                    console.log(error);
+                });
+
+            }
+ 
+        };
+
+
+
+        // vm.search = function () {
+        //     console.log("bouton cliqué", vm.searchQuery);
+        //     if (vm.searchQuery) {
+        //         var dateFormat = 'yyyy-MM-dd';
+        //         var searchQuery = $filter('date')(vm.searchQuery, dateFormat);
+        //         console.log(API_URL + 'api/search/'+searchQuery+'/inventaires?page=' + (vm.currentPage ?? 0) + '&size=' + (vm.pageSize ?? 3));
+        //         FetchData.getData(API_URL + 'api/search/'+searchQuery+'/inventaires?page=' + (vm.currentPage ?? 0) + '&size=' + (vm.pageSize ?? 3))
+        //         .then(function (response) {
+        //             console.log(response);
+        //             vm.inventaires = response.data.content;
+        //             vm.totalElements = response.data.totalElements;
+        //             vm.totalPage = response.data.totalPages;
+
+        //             console.log('nombre d\'élément ' + vm.totalElements);
+        //             console.log('nombre de page ' + vm.totalPage);
+        //             console.log("OUUHHH "+vm.inventaires);
+        //         }, function (error) {
+        //             console.log(error);
+        //         });
+
+        //     }
+ 
+        // };
         vm.loadAll();
 
 
@@ -73,6 +133,55 @@
             window.open(url);
         }
 
+
+
+
+        vm.datePickerOpenStatus = {};
+        vm.datePickerOpenStatus.searchAnnee = false;
+        vm.datePickerOpenStatus.searchDate = false;
+    
+        vm.openCalendar = function(date) {
+            vm.datePickerOpenStatus[date] = true;
+        };
         
+
+
+        
+
+
+        // vm.dt = new Date();
+        //         vm.format = 'yyyy';
+        //         vm.altInputFormats = ['yyyy'];
+                vm.options = {
+                    minMode: 'year',
+                    datepickerMode: 'year',
+                    formatYear: 'yyyy',
+                    maxDate: new Date(),
+                    startingDay: 1,
+                    showWeeks: false
+                };
+
+
+                vm.options2 = {
+                    minMode: 'month',
+                    datepickerMode: 'month',
+                    formatMonth: 'MMMM',
+                    formatYear: 'yyyy',
+                    maxDate: new Date(),
+                    startingDay: 1,
+                    showWeeks: false
+                };
+
+                // vm.popup = {
+                //     opened: false
+                // };
+
+                // vm.open = function() {
+                //     console.log("OPEN");
+                //     vm.popup.opened = true;
+                // };
+
+
+
     }
 })();
