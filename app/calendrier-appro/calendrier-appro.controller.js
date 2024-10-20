e14a0fe3159cc926357dfdfe8db787315ebc5e8e
unknown <isaacndri@gmail.com>
2024-10-20 16:00:26 +0000

    26 clean code

--- app/calendrier-appro/calendrier-appro.controller.js
+++ app/calendrier-appro/calendrier-appro.controller.js
@@ -4,10 +4,7 @@
     angular
         .module('app')
         .controller('CalendrierApproController', CalendrierApproController);
-        /*'Lignefacture', 'LignefactureSearch'*/
         CalendrierApproController.$inject = ['$scope', '$state', 'FetchData', 'API_URL', 'AuthServerProvider', '$filter'];
-
-    /*Lignefacture, LignefactureSearch*/
     function CalendrierApproController ($scope, $state, FetchData, API_URL, AuthServerProvider, $filter) {
 
         var vm = this;
@@ -17,9 +14,10 @@
         vm.searchAnnee = '';
         vm.typeRecherche = 'bymoisannee'
         var authToken = AuthServerProvider.getToken();
+
         vm.loadAll = function() {
             
-            FetchData.getData(API_URL + 'api/_page/inventaires') //?page=' + (vm.currentPage ?? 0) + '&size=' + (vm.pageSize ?? 3))
+            FetchData.getData(API_URL + 'api/_page/inventaires')
             .then(function (response) {
                 console.log("inventaires @@@@@@@@@@@@@@@", response);
                 vm.inventaires = response.data;
@@ -39,11 +37,9 @@
 
         };
 
-
         vm.searchQuery = '';
         vm.search = function () {
 
-      
         console.log("bouton cliqué", vm.searchQuery);
         if (vm.searchQuery) {
             var dateFormat = 'yyyy-MM-dd';
@@ -60,8 +56,6 @@
             });
 
         }
-
-
  
         };
 
@@ -87,7 +81,6 @@
 
         vm.loadAll();
 
-
         vm.exportExcel = function () {
                 var dateFormat = 'yyyy-MM-dd';
                 var searchQuery = $filter('date')(new Date(), dateFormat);
@@ -95,11 +88,6 @@
                 var url = API_URL + 'api/report/inventaires/calendrier-approvisionnement/'+searchQuery/* + '?access_token=' + authToken*/;
                 location.assign(url);
         }
-
-
-        
-
-
 
         vm.datePickerOpenStatus = {};
         vm.datePickerOpenStatus.searchAnnee = false;
@@ -109,36 +97,28 @@
             vm.datePickerOpenStatus[date] = true;
         };
         
-
         vm.changeOptions = function() {
             return vm.produit.id ? vm.options : vm.options2;
         }
 
-
-        
-
-
-                vm.options = {
-                    minMode: 'year',
-                    datepickerMode: 'year',
-                    formatYear: 'yyyy',
-                    maxDate: new Date(),
-                    startingDay: 1,
-                    showWeeks: false
-                };
+        vm.options = {
+            minMode: 'year',
+            datepickerMode: 'year',
+            formatYear: 'yyyy',
+            maxDate: new Date(),
+            startingDay: 1,
+            showWeeks: false
+        };
 
 
-                vm.options2 = {
-                    minMode: 'month',
-                    datepickerMode: 'month',
-                    formatMonth: 'MMMM',
-                    formatYear: 'yyyy',
-                    maxDate: new Date(),
-                    startingDay: 1,
-                    showWeeks: false
-                };
-
-
-
+        vm.options2 = {
+            minMode: 'month',
+            datepickerMode: 'month',
+            formatMonth: 'MMMM',
+            formatYear: 'yyyy',
+            maxDate: new Date(),
+            startingDay: 1,
+            showWeeks: false
+        };
     }
 })();
